7e8f90cc24475bebb26d6b01a88e8836051c9045
akunbaru aja <akunbaruajaah@gmail.com>
2024-07-29 07:55:40 +0000

    akunbaruajaahehehe

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x4dd44Ee19bB0C0aba12aCB9d48C4808b3dBf238a'
+export const ProxyAdmin = '0x280F62466255dbFc28B92Fb7dd538FDffc9c3271'
+export const SWTRImplementation = '0x4C84E5E4716470f27B1Ba7c78580FE0c667e2151'
     
